d32c9bd92cd823209f2414c7ad648da3944f7c92
Patrick Carrington <pcarring@SNOWBALL.SP.CS.CMU.EDU>
2018-06-06 11:52:48 -0400

    updates to v0

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,16 +1,53 @@
-var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('video-placeholder', {
-        width: 600,
-        height: 400,
-        videoId: 'Xa0Q0J5tOP0',
-        playerVars: {
-            color: 'white',
-            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
-        },
-        events: {
-            onReady: initialize
-        }
-    });
-}
+
+$('#mute-toggle').on('click', function() {
+    var mute_toggle = $(this); // The button
+
+    if(player.isMuted()){
+        player.unMute();
+        mute_toggle.text('P1 mute');
+    }
+    else{
+        player.mute();
+        mute_toggle.text('P1 un-mute');
+    }
+});
+
+$('#mute-toggle1').on('click', function() {
+    var mute_toggle = $(this); // The button
+
+    if(player1.isMuted()){
+        player1.unMute();
+        mute_toggle.text('P2 mute');
+    }
+    else{
+        player1.mute();
+        mute_toggle.text('P2 un-mute');
+    }
+});
+
+$('#mute-toggle2').on('click', function() {
+    var mute_toggle = $(this); // The button
+
+    if(player2.isMuted()){
+        player2.unMute();
+        mute_toggle.text('P3 mute');
+    }
+    else{
+        player2.mute();
+        mute_toggle.text('P3 un-mute');
+    }
+});
+
+$('#mute-toggle3').on('click', function() {
+    var mute_toggle = $(this); // The button
+
+    if(player3.isMuted()){
+        player3.unMute();
+        mute_toggle.text('P4 mute');
+    }
+    else{
+        player3.mute();
+        mute_toggle.text('P4 un-mute');
+    }
+});
