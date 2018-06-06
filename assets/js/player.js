a0589076f0413c41794d7aa5c6d04c9c0a1769ea
Patrick Carrington <pcarring@SNOWBALL.SP.CS.CMU.EDU>
2018-06-05 21:42:19 -0400

    more updates

---
+++ assets/js/player.js
@@ -0,0 +1,16 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: 'Xa0Q0J5tOP0',
+        playerVars: {
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
